7ee3dd55b179cba58219587948d9267f66007d09
Blake Miller <blak3mill3r@gmail.com>
2012-09-25 08:42:58 -0700

    add a way to get paged results

--- lib/bigquery.rb
+++ lib/bigquery.rb
@@ -71,12 +71,21 @@ class BigQuery
   # perform a query synchronously
   # fetch all result rows, even when that takes >1 query
   # invoke /block/ once for each row, passing the row
-  def each(q, &block)
+  def each_row(q, &block)
     current_row = 0
     # repeatedly fetch results, starting from current_row
     # invoke the block on each one, then grab next page if there is one
     # it'll terminate when res has no 'rows' key or when we've done enough rows
-    while( ( res = query(q, :startIndex => current_row) ) && res['rows'] && current_row < res['totalRows'] ) do
+    # perform query...
+    res = query(q)
+    job_id = res['jobReference']['jobId']
+    # call the block on the first page of results
+    if( res && res['rows'] )
+      res['rows'].each(&block)
+      current_row += res['rows'].size
+    end
+    # keep grabbing pages from the API and calling the block on each row
+    while(( res = get_query_results(job_id, :startIndex => current_row) ) && res['rows'] && current_row < res['totalRows'].to_i ) do
       res['rows'].each(&block)
       current_row += res['rows'].size
     end
