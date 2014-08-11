89bb0cc725a0792b50b5fda6c1eb1bb81dbcf46e
David Salgado <david@digitalronin.com>
2014-08-11 17:31:31 +0100

    accept options hash in each_row and pass through to query

--- lib/bigquery.rb
+++ lib/bigquery.rb
@@ -79,13 +79,13 @@ class BigQuery
   # perform a query synchronously
   # fetch all result rows, even when that takes >1 query
   # invoke /block/ once for each row, passing the row
-  def each_row(q, &block)
+  def each_row(q, options = {}, &block)
     current_row = 0
     # repeatedly fetch results, starting from current_row
     # invoke the block on each one, then grab next page if there is one
     # it'll terminate when res has no 'rows' key or when we've done enough rows
     # perform query...
-    res = query(q)
+    res = query(q, options)
     job_id = res['jobReference']['jobId']
     # call the block on the first page of results
     if( res && res['rows'] )
