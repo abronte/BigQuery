736b693a219f4619853ebfc45eabbd2c9de845db
andresbravog <andresbravog@gmail.com>
2014-08-17 00:09:45 +0200

    Restored digitalronin changes

--- lib/big_query/client/query.rb
+++ lib/big_query/client/query.rb
@@ -4,12 +4,15 @@ module BigQuery
       # Performs the given query in the bigquery api
       #
       # @param given_query [String] query to perform
+      # @param options [Hash] query options
+      # @option options [Integer] timeout (90 * 1000) timeout in miliseconds
       # @return [Hash] json api response
-      def query(given_query)
+      def query(given_query, options={})
+        timeout = options.fetch(:timeout, 90 * 1000)
         response = api(
           api_method: @bq.jobs.query,
           body_object: { 'query' => given_query,
-                         'timeoutMs' => 90 * 1000}
+                         'timeoutMs' => timeout}
         )
 
         response
@@ -18,13 +21,17 @@ module BigQuery
       # perform a query synchronously
       # fetch all result rows, even when that takes >1 query
       # invoke /block/ once for each row, passing the row
-      def each_row(q, &block)
+      #
+      # @param q [String] query to be executed
+      # @param options [Hash] query options
+      # @option options [Integer] timeout (90 * 1000) timeout in miliseconds
+      def each_row(q, options = {}, &block)
         current_row = 0
         # repeatedly fetch results, starting from current_row
         # invoke the block on each one, then grab next page if there is one
         # it'll terminate when res has no 'rows' key or when we've done enough rows
         # perform query...
-        res = query(q)
+        res = query(q, options)
         job_id = res['jobReference']['jobId']
         # call the block on the first page of results
         if( res && res['rows'] )
