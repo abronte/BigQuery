0b86b69b12a14af0136319157d91a3a18fdceae0
Marcus Derencius <derencius@gmail.com>
2015-01-08 13:13:22 -0200

    Adding support to RECORD type field.

--- lib/big_query/client/tables.rb
+++ lib/big_query/client/tables.rb
@@ -108,7 +108,7 @@ module BigQuery
         )
       end
 
-      protected
+      # protected
 
       # Translate given schema to a one understandable by bigquery
       #
@@ -121,6 +121,9 @@ module BigQuery
           mode = (ALLOWED_FIELD_MODES & [options[:mode].to_s]).first
           field = { "name" => name.to_s, "type" => type }
           field["mode"] = mode if mode
+          if field["type"] == 'RECORD'
+            field["fields"] = validate_schema(options[:fields])
+          end
           fields << field
         end
         fields
